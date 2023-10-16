c71665fd8379a1f689d6fde9a136858236364b0a
smednickd <132397271+smednickd@users.noreply.github.com>
2023-10-16 10:12:06 -0700

    added metadata configuration

--- src/main/java/com/deloitte/elrr/CacheConfig.java
+++ src/main/java/com/deloitte/elrr/CacheConfig.java
@@ -5,12 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class CacheConfig {
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    http
+            .saml2Metadata(withDefaults())
+            .saml2Login(withDefaults());
         return http.build();
     }
 }
