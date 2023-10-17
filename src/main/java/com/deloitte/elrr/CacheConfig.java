0d90b306702c955658398f830a48755dbb8d27fa
JDTobin <90791836+JDTobin@users.noreply.github.com>
2023-10-17 11:39:33 -0400

    use metadata url for authentication config and add authorization

--- src/main/java/com/deloitte/elrr/CacheConfig.java
+++ src/main/java/com/deloitte/elrr/CacheConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.saml2.provider.service.registration.InMemoryRelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrations;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -11,10 +15,25 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class CacheConfig {
 	@Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	    http
-            .saml2Metadata(withDefaults())
-            .saml2Login(withDefaults());
-        return http.build();
-    }
+	public RelyingPartyRegistrationRepository relyingPartyRegistrations() throws Exception {
+		RelyingPartyRegistration relyingPartyRegistration = RelyingPartyRegistrations
+				.fromMetadataLocation("https://samltest.id/saml/idp").registrationId("samltest").build();
+
+		return new InMemoryRelyingPartyRegistrationRepository(relyingPartyRegistration);
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()).saml2Metadata(withDefaults())
+				.saml2Login(saml2 -> {
+					try {
+						saml2.relyingPartyRegistrationRepository(relyingPartyRegistrations());
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				});
+
+		return http.build();
+	}
 }
