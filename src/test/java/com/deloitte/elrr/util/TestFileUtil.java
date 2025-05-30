eb9d2c30ee8f82c10b5fafbc11f43d1509016421
Leven <phleven@deloitte.com>
2025-05-29 20:18:32 -0400

    Linting anf JUnit test.

---
+++ src/test/java/com/deloitte/elrr/util/TestFileUtil.java
@@ -0,0 +1,34 @@
+package com.deloitte.elrr.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.core.io.ClassPathResource;
+
+public final class TestFileUtil {
+
+    /**
+     * @param filename
+     * @return file
+     * @throws IOException
+     */
+    public static File getJsonTestFile(String filename)
+            throws IOException {
+
+        File file;
+
+        try {
+            file = new ClassPathResource(filename).getFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+        return file;
+    }
+
+    private TestFileUtil() {
+        throw new UnsupportedOperationException(
+                "This is a utility class and cannot be instantiated");
+    }
+}
