5df2c16db225262e882d2cc83537acc456731d83
Leven <phleven@deloitte.com>
2025-06-12 10:26:54 -0400

    Refactor TestFileUtil

--- src/test/java/com/deloitte/elrr/util/TestFileUtil.java
+++ src/test/java/com/deloitte/elrr/util/TestFileUtil.java
@@ -13,16 +13,7 @@ public final class TestFileUtil {
      * @throws IOException
      */
     public static File getJsonTestFile(String filename) throws IOException {
-
-        File file;
-
-        try {
-            file = new ClassPathResource(filename).getFile();
-        } catch (IOException e) {
-            throw e;
-        }
-
-        return file;
+        return new ClassPathResource(filename).getFile();
     }
 
     private TestFileUtil() {
